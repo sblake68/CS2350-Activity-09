44a3652c613f2ed79d9b7a8cdc98c217dd851d8a
sbake68 <shawnblake@mail.weber.edu>
2020-04-10 10:23:33 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,82 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import 'bootstrap'
+
+function displayCard(c){
+    return `
+    <div class="card" data-title="${c.title}">
+        <img src="${c.poster}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${c.title}</h5>
+            <p class="card-text">${c.description}</p>
+            <button class="btn btn-danger">Delete</a>
+        </div>
+    </div>
+  `
+}
+
+function displayCards(){
+    let cards = JSON.parse(localStorage.getItem('cards') || '[]')
+
+    document.querySelector('#cards').innerHTML = ''
+    for(let c of cards){
+        let col = document.createElement('div')
+        col.setAttribute('class', 'col-md-4')
+        col.innerHTML = displayCard(c)
+        document.querySelector('#cards').appendChild(col)
+    }
+
+    document.querySelectorAll('button.btn-danger').forEach(function(b){
+        b.onclick = function(enevent){
+            let cards = JSON.parse(localStorage.getItem('cards') || '[]')
+            let ndx = -1
+            for (let i in cards) {
+                if(cards[i].title === event.target.closest('.card').dataset.title){
+                    ndx= i 
+                    break
+                }
+            }
+            if(ndx != -1){
+                cards.splice(ndx,1)
+                localStorage.setItem('cards', JSON.stringify(cards))
+                location.reload()
+            }
+        }
+    })
+
+}
+
+function addNewCard(event){
+    if(event) event.preventDefault()
+
+    let t = document.querySelector('#title').value
+    let d = document.querySelector('#description').value
+    let p = document.querySelector('#poster').value
+
+    let cards = JSON.parse(localStorage.getItem('cards') || '[]')
+
+    if(t && d && p){
+        let card = {title: t, description: d, poster: p}
+        cards.push(card)
+        localStorage.setItem('cards', JSON.stringify(cards))
+    }
+   
+    document.querySelector('#cards').classList.remove('d-none')
+    document.querySelector('#myForm').classList.add('d-none')
+
+    displayCards() 
+
+
+}
+
+document.querySelector('#new_card').onclick = function(){
+    document.querySelector('#myForm').classList.remove('d-none')
+    document.querySelector('#cards').classList.add('d-none')
+}
+document.forms[1].querySelector('[type="button"]').onclick = function(){
+    document.querySelector('#cards').classList.remove('d-none')
+    document.querySelector('#myForm').classList.add('d-none')
+}
+document.forms[1].addEventListener('submit', addNewCard, false)
+displayCards()
